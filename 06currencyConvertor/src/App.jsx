ea6e09923661c3729c1b70d998322c8c3fab0385
Mayankpuri1000 <mayankpuri1000@gmail.com>
2025-02-01 13:01:00 +0530

    modified file

--- 06currencyConvertor/src/App.jsx
+++ 06currencyConvertor/src/App.jsx
@@ -43,7 +43,7 @@ function App() {
                 label="From"
                 amount={amount}
                 currencyOptions={options}
-                onCurrencyChange={(currency) => setAmount(amount)}
+                onCurrencyChange={(amount) => setAmount(amount)}
                 selectCurrency={from}
                 onAmountChange={(amount) => setAmount(amount)}
               />
